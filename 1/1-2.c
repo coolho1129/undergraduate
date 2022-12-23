f364353b908aedda7d0942fe6d4ddd0813deae87
COOLHO <coolho123@naver.com>
2022-12-24 00:46:21 +0900

    add 1 folder

---
+++ 1/1-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    double x = 3.141592, y = 5.435231;
+    int a = 32, b = 56;
+    char c = 'b', d = 'c';
+
+
+    printf("%d %d\n",a,b);
+    printf("%f %f\n",x,y);
+    printf("%c %c\n",c,d);
+  
+
+}
