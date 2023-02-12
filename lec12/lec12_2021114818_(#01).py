236b654fc5de5c8a30965af856e10c8f859ed8f4
CoolHo1129 <coolho123@knu.ac.kr>
2023-02-12 17:43:30 +0900

    modified lec12

--- lec12/lec12_2021114818_(#01).py
+++ lec12/lec12_2021114818_(#01).py
@@ -1,6 +1,6 @@
 import time
 now = time.localtime()
-number=input('주민등록번호 년월일:')
+number=input('주민등록번호 년월일: ')
 
 
 age = now.tm_year-(int(number[0:2])+1900)+1
--- lec12/lec12_2021114818_(#03).py
+++ lec12/lec12_2021114818_(#03).py
@@ -1,7 +1,7 @@
-jnymd=input('주민등록번호 년월일:' )
+jnymd=input('주민등록번호 년월일: ' )
 if 00<int(jnymd[2:4])<13:
     if 00<int(jnymd[4:6])<32:
-        mf=input('성별표시 :')
+        mf=input('성별표시 : ')
 
     else:
         print('주민번호 일 형식이 잘못되었습니다.')
