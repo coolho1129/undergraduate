02ab7c2126fc52a49d1b6c15614800fb8ce7a16c
CoolHo1129 <coolho123@knu.ac.kr>
2023-10-31 04:45:26 +0900

    modified resolution_down.py 원본 영상 비율에 맞추어 다운스케일링 하도록 코드를 수정함

--- resolution_down.py
+++ resolution_down.py
@@ -1,11 +1,21 @@
 import ffmpeg
 import os
 import sys
+import cv2
+
 def resize(src,mode):
+    # 비디오 파일 열기
+    cap = cv2.VideoCapture(src)
+
+    # 비디오 파일 정보 얻기
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 비디오 프레임의 너비
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 비디오 프레임의 높이
     
-    # 해상도 변경 모드 설정
-    resolution={'FHD':'scale=1920:1080','HD':'scale=1280:720'}
-    #print(resolution[mode])
+    if mode=='FHD':
+        resolution='scale=1920:'+str(int((height/width)*1920))
+    else:
+        resolution='scale=1280:'+str(int((height/width)*1280))
+
     
     #생성파일 저장 폴더 생성
     dstsrc=src.replace("MIV_mp4", "MIV_mp4_resize")
@@ -21,16 +31,17 @@ def resize(src,mode):
     input_stream = ffmpeg.input(input_file)
 
     # 비디오 스트림을 해상도 변경
-    output_stream = ffmpeg.output(input_stream, output_file, vf=resolution[mode])
+    output_stream = ffmpeg.output(input_stream, output_file, vf=resolution)
 
     # 오디오 스트림을 복사
-    output_stream = ffmpeg.output(output_stream, vf=resolution[mode], acodec='copy')
+    output_stream = ffmpeg.output(output_stream, vf=resolution, acodec='copy')
 
     # FFmpeg 명령 실행
     ffmpeg.run(output_stream)
     
   
 def main(mivmp4_src):
+
     mode="FHD"
     argc=len(sys.argv)
     if(argc==3 and sys.argv[2].upper()=='HD'):
