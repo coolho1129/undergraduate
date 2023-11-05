0106fde474b901084447405cf14f97293a97123c
CoolHo1129 <coolho123@knu.ac.kr>
2023-11-06 00:44:01 +0900

    파일정리

---
+++ ORPVR_METAVERSE/resolution_down.py
@@ -0,0 +1,71 @@
+import ffmpeg
+import os
+import sys
+import cv2
+
+def resize(src,mode):
+    # 비디오 파일 열기
+    cap = cv2.VideoCapture(src)
+
+    # 비디오 파일 정보 얻기
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 비디오 프레임의 너비
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 비디오 프레임의 높이
+    
+    print('현재 해상도: {} x {}'.format(width, height))
+    if mode=='FHD':
+        resolution='scale=1920:'+str(int((height/width)*1920))
+    else:
+        resolution='scale=1280:'+str(int((height/width)*1280))
+    print('변경 해상도: {}'.format(resolution))
+    
+
+    
+    #생성파일 저장 폴더 생성
+    dstsrc=src.replace("MIV_mp4", "MIV_mp4_resize")
+    dstsrc=src.replace(dstsrc.split("\\")[1],dstsrc.split("\\")[1]+"_resize")
+    dstdir=dstsrc.replace(dstsrc.split("\\")[-1],"")
+    os.makedirs(dstdir,exist_ok=True)
+    
+    # 입력 및 출력 파일 경로 설정
+    input_file = src
+    output_file = os.path.join(dstsrc)
+        
+    # 입력 동영상 스트림 생성
+    input_stream = ffmpeg.input(input_file)
+
+    # 비디오 스트림을 해상도 변경
+    output_stream = ffmpeg.output(input_stream, output_file, vf=resolution)
+
+    # 오디오 스트림을 복사
+    output_stream = ffmpeg.output(output_stream, vf=resolution, acodec='copy')
+
+    # FFmpeg 명령 실행
+    ffmpeg.run(output_stream)
+    print('downscale complete')
+    
+  
+def main(mivmp4_src):
+
+    mode="FHD"
+    argc=len(sys.argv)
+    if(argc==3 and sys.argv[2].upper()=='HD'):
+        mode='HD'
+    if(argc>3 or argc<2):
+        print("아래와 같은 형태로 인자를 입력해주세요(mode:FHD or HD)")
+        print("python resolution_down.py src mode")
+        exit(-1)
+    
+    high_dir=os.listdir(mivmp4_src)
+    for high in high_dir:
+        high_path=os.path.join(mivmp4_src,high)
+        low_dir=os.listdir(high_path)
+        for low in low_dir:
+            low_path=os.path.join(high_path,low)
+            videos=os.listdir(low_path)
+            for video in videos:
+                video_path=os.path.join(low_path,video)
+                resize(video_path,mode)
+
+if __name__ == '__main__':
+    #print(sys.argv)
+    main(sys.argv[1])
