f4453fa7c005d72d2ec9027c0902fed41ef3855c
CoolHo(김찬호) <coolho123@knu.ac.kr>
2023-11-06 15:45:07 +0900

    Update resolution_down.py

--- ORPVR_METAVERSE/resolution_down.py
+++ ORPVR_METAVERSE/resolution_down.py
@@ -2,26 +2,28 @@ import ffmpeg
 import os
 import sys
 import cv2
+from PIL import Image
 
-def resize(src,mode):
+def video_resize(src,mode):
     # 비디오 파일 열기
     cap = cv2.VideoCapture(src)
 
     # 비디오 파일 정보 얻기
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 비디오 프레임의 너비
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 비디오 프레임의 높이
-    
     print('현재 해상도: {} x {}'.format(width, height))
+    resolution=""
     if mode=='FHD':
         resolution='scale=1920:'+str(int((height/width)*1920))
     else:
         resolution='scale=1280:'+str(int((height/width)*1280))
     print('변경 해상도: {}'.format(resolution))
     
-
     
     #생성파일 저장 폴더 생성
-    dstsrc=src.replace("MIV_mp4", "MIV_mp4_resize")
+    folder=src.split("\\")[0]
+    dstfolder=folder+'_resize'
+    dstsrc=src.replace(folder, dstfolder)
     dstsrc=src.replace(dstsrc.split("\\")[1],dstsrc.split("\\")[1]+"_resize")
     dstdir=dstsrc.replace(dstsrc.split("\\")[-1],"")
     os.makedirs(dstdir,exist_ok=True)
@@ -32,17 +34,38 @@ def resize(src,mode):
         
     # 입력 동영상 스트림 생성
     input_stream = ffmpeg.input(input_file)
-
-`   # 출력 동영상 스트림 생성
     output_stream = ffmpeg.output(input_stream, output_file, vf=resolution, vcodec='libx264',acodec='copy')
 
     # FFmpeg 명령 실행
     ffmpeg.run(output_stream)
-    print('downscale complete')
+    print('video downscale complete')
+
+def img_resize(src):
+    #생성파일 저장 폴더 생성
+    folder=src.split("\\")[0]
+    dstfolder=folder+'_resize'
+    dstsrc=src.replace(folder, dstfolder)
+    dstsrc=src.replace(dstsrc.split("\\")[1],dstsrc.split("\\")[1]+"_resize")
+    dstdir=dstsrc.replace(dstsrc.split("\\")[-1],"")
+    os.makedirs(dstdir,exist_ok=True)
+    
+    # 입력 및 출력 파일 경로 설정
+    input_file = src
+    output_file = os.path.join(dstsrc)
+    # 이미지 불러오기
+    input_image = Image.open(input_file)
+
+    # 원하는 해상도로 이미지 크기 조정
+    new_width = 1280
+    new_height = 720
+    resized_image = input_image.resize((new_width, new_height), Image.ANTIALIAS)
+
+    # 조정된 이미지 저장
+    resized_image.save(output_file)
+    print('image resizing complete')
     
   
-def main(mivmp4_src):
-
+def main(src):
     mode="FHD"
     argc=len(sys.argv)
     if(argc==3 and sys.argv[2].upper()=='HD'):
@@ -52,16 +75,19 @@ def main(mivmp4_src):
         print("python resolution_down.py src mode")
         exit(-1)
     
-    high_dir=os.listdir(mivmp4_src)
+    high_dir=os.listdir(src)
     for high in high_dir:
-        high_path=os.path.join(mivmp4_src,high)
+        high_path=os.path.join(src,high)
         low_dir=os.listdir(high_path)
         for low in low_dir:
             low_path=os.path.join(high_path,low)
-            videos=os.listdir(low_path)
-            for video in videos:
-                video_path=os.path.join(low_path,video)
-                resize(video_path,mode)
+            files=os.listdir(low_path)
+            for file in files:
+                file_path=os.path.join(low_path,file)
+                if file.lower().endswith('.jpg') or file.lower().endswith('.png'):
+                    img_resize(file_path)
+                elif file.lower().endswith('.mp4'):
+                    video_resize(file_path,mode)
 
 if __name__ == '__main__':
     #print(sys.argv)
