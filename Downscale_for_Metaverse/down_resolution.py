24d15eee0c83fd37f3a0aabdc2c3284b0ca98a1d
CoolHo(김찬호) <coolho123@knu.ac.kr>
2023-11-28 16:15:14 +0900

    Update down_resolution.py

--- Downscale_for_Metaverse/down_resolution.py
+++ Downscale_for_Metaverse/down_resolution.py
@@ -123,7 +123,7 @@ def main():
     parser.add_argument('-input','-i', type=str, help='이미지 또는 비디오 파일 경로를 입력해주세요.')
     parser.add_argument('-output','-o', type=str, default='.', help='출력 파일 경로를 입력해주세요.')
     parser.add_argument('-mode','-m', choices=['FHD', 'HD', 'SD'], default='FHD', help='해상도 모드 설정 (기본값: FHD)')
-    parser.add_argument('-ratio','-r', type=str, help='다운 스케일 비율 설정, N:M 형식으로 입력해주세요. (기본값: 원본 비율)')
+    parser.add_argument('-ratio','-r', type=str, help='다운 스케일 비율 설정, N:M 형식으로 입력해주세요. (기본값: 원본 비율 유지)')
     
     args = parser.parse_args()
     if not args.input:
