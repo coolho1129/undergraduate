62bac94410d78a2e7144b4865a3a50721b411827
CoolHo(김찬호) <coolho123@naver.com>
2023-11-29 20:34:21 +0900

    Update down_resolution.py

--- Downscale_for_Metaverse/down_resolution.py
+++ Downscale_for_Metaverse/down_resolution.py
@@ -121,7 +121,7 @@ def img_downscale(input,output,mode,ratio):
 def main():
     parser = argparse.ArgumentParser(description='명령줄에서 해상도 설정을 변경하는 스크립트')
     parser.add_argument('-input','-i', type=str, help='이미지 또는 비디오 파일 경로를 입력해주세요.')
-    parser.add_argument('-output','-o', type=str, default='.', help='출력 파일 경로를 입력해주세요. (기본 경로: 현재 폴더')
+    parser.add_argument('-output','-o', type=str, default='.', help='출력 파일 경로를 입력해주세요. (기본 값: 현재 폴더')
     parser.add_argument('-mode','-m', choices=['FHD', 'HD', 'SD'], default='FHD', help='해상도 모드(FHD,HD,SD)를 대문자로 입력해주세요. (기본값: FHD)')
     parser.add_argument('-ratio','-r', type=str, help='다운 스케일 비율 설정, N:M 형식으로 입력해주세요. (기본값: 원본 비율 유지)')
     
