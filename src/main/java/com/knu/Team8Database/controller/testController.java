ff720cb78140bd3fa2186f384fb95ab089930950
송혜경 <sosschs9@naver.com>
2023-12-03 23:08:47 +0900

    create detail.html

--- src/main/java/com/knu/Team8Database/controller/testController.java
+++ src/main/java/com/knu/Team8Database/controller/testController.java
@@ -1,13 +1,23 @@
 package com.knu.Team8Database.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @Controller
 public class testController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "index";
-    }
+//    @PostMapping("/example")
+//    public String hello(Model model) {
+//
+//        // resDTO 안에 있는 변수 사용해서 쿼리문 요청
+//        Map<String, String> itemMap = new HashMap<>();
+//        itemMap.put("", "");
+//        // 요청 값 Map이나 List로 구성
+//        model.addAttribute("responseData", itemMap);
+//
+//        return "index";
+//    }
 }
