9bb45f8c57c45c5c9437e397ddfe73af89f0cee2
coolho <coolho123@knu.ac.kr>
2023-12-04 06:24:13 +0900

    update repository

--- src/main/java/com/knu/Team8Database/repository/MedicineRepository.java
+++ src/main/java/com/knu/Team8Database/repository/MedicineRepository.java
@@ -16,20 +16,21 @@ import java.util.List;
 public interface MedicineRepository extends JpaRepository<Detail_view, String> {
 
 
-    @Query("SELECT D.medicineId, D.medicineName, D.medicineCapacity, D.medicinePrice, D.medicineManufactureDate, " +
+    @Query("SELECT DISTINCT D.medicineId, D.medicineName, D.medicineCapacity, D.medicinePrice, D.medicineManufactureDate, " +
         "D.companyName, D.companyPhoneNumber, D.companyWebsite, D.symtomName, D.symtomField, " +
         "D.componentName, D.componentSideEffects FROM Detail_view D " +
         "WHERE (:medicine_id IS NULL OR D.medicineId = :medicine_id)"
 )
     List<Detail_viewDTO> find_detail(@Param("medicine_id") String medicine_id);
 
-    @Query("SELECT D.medicineId AS medicineId, D.medicineName AS medicineName, D.medicineCapacity AS medicineCapacity, D.symtomName AS symtomName, D.companyName AS companyName " +
+    @Query("SELECT DISTINCT D.medicineId AS medicineId, D.medicineName AS medicineName, D.medicineCapacity AS medicineCapacity, D.symtomName AS symtomName, D.companyName AS companyName " +
             "FROM Detail_view D WHERE (D.medicineName LIKE %:medicineName%) AND (D.symtomName LIKE %:symtomName%)" +
             " AND (D.companyName LIKE %:companyName%) AND (D.medicineCapacity LIKE %:medicineCapacity%)" +
             " AND (D.medicinePrice >= :medicinePrice) AND (D.symtomField LIKE %:symtomField%)")
     List<Detail_viewDTO> find_simple(@Param("medicineName")String medicineName, @Param("medicineCapacity")String medicineCapacity,
                                      @Param("symtomName")String symtomName, @Param("companyName")String companyName,
                                      @Param("medicinePrice")Integer medicinePrice, @Param("symtomField")String symtomField);
+
 
 
 }
